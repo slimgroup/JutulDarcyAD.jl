09fcec9fb9de22af6a3b5f0b493e022f6723d078
Grant Bruer <grant.git.email@gmail.com>
2025-01-17 15:59:08 -0500

    Added rrule for simulate()

--- src/rrule_simulate.jl
+++ src/rrule_simulate.jl
@@ -1,11 +1,26 @@
 using ChainRulesCore: rrule, unthunk, NoTangent, @not_implemented
-using Jutul: simulate, setup_parameter_optimization, optimization_config, vectorize_variables
+using Jutul: simulate, setup_parameter_optimization, optimization_config, vectorize_variables, SimulationModel, MultiModel, submodels, get_primary_variables
 using JutulDarcy
 
 function simulate_ad(state0, model, tstep, parameters, forces; opt_config=nothing, kwargs...)
     # TODO: should help user by erroring if kwargs also specifies parameters and forces.
     return simulate(state0, model, tstep; kwargs..., parameters, forces);
 end
+
+get_state_keys(model::SimulationModel, state::Dict{Any, Any}) = keys(state)
+function get_state_keys(model::MultiModel, state::Dict{Any, Any})
+    state_keys = Dict{Any, Any}()
+    for (k, m) in pairs(submodels(model))
+        state_keys[k] = keys(state[k])
+    end
+    return state_keys
+end
+
+get_eltype(model::SimulationModel, state) = eltype(state.Saturations)
+get_eltype(model::MultiModel, state) = get_eltype(model[:Reservoir], state.Reservoir)
+
+get_eltype(model::SimulationModel, state::Dict{Any, Any}) = eltype(state[:Saturations])
+get_eltype(model::MultiModel, state::Dict{Any, Any}) = get_eltype(model[:Reservoir], state[:Reservoir])
 
 function ChainRulesCore.rrule(::typeof(simulate_ad), state0, model, tstep, parameters, forces; opt_config=nothing, kwargs...)
     output = simulate_ad(state0, model, tstep, parameters, forces; kwargs...);
@@ -20,21 +35,37 @@ function ChainRulesCore.rrule(::typeof(simulate_ad), state0, model, tstep, param
         #   - Choose c = dy - M(x).
         #   - Then dF/dx = Jᵀdy as desired.
 
+        @show typeof(doutput)
         dstates = unthunk(unthunk(doutput).states)
+        @show typeof(dstates)
+        @show typeof(dstates[1])
+        @show Jutul.variable_mapper(model, :primary)
 
         # 1. First, we define F, which needs to subtract two Jutul states.
         #   This is easier to do if the states are vectorized, so we'll set
         #   up a vectorizer first. It should be restricted to the targets
         #   that are nonzero in dstates.
 
-        targets = keys(first(dstates))
+        targets = get_state_keys(model, first(dstates))
+        @show targets
         mapper = first(Jutul.variable_mapper(model, :primary; targets))
         function F(model, state_ad, dt, step_no, forces)
             # Vectorize everything (with the right type).
-            T = eltype(state_ad.Saturations[1,1])
+            # @show get_eltype(model, dstates[step_no])
+            # T = get_eltype(model, dstates[step_no])
+            T = Real
             d_state_vec = vectorize_variables(model, dstates[step_no], mapper; T)
+
+            # @show get_eltype(model, states[step_no])
+            # T = get_eltype(model, states[step_no])
+            T = Real
             state_vec = vectorize_variables(model, states[step_no], mapper; T)
+
+            # @show get_eltype(model, state_ad)
+            # T = get_eltype(model, state_ad)
+            T = Real
             state_ad_vec = vectorize_variables(model, state_ad, mapper; T)
+
             c = d_state_vec - state_vec
             return sum((state_ad_vec + c) .^ 2) / 2
         end
@@ -52,7 +83,7 @@ function ChainRulesCore.rrule(::typeof(simulate_ad), state0, model, tstep, param
                 end
             end
         end
-        opt = setup_parameter_optimization(model, state0, parameters, tstep, forces, F, opt_config, print = 100, param_obj = true);
+        opt = setup_parameter_optimization(model, state0, parameters, tstep, forces, F, opt_config, print = 100, param_obj = true, use_sparsity=true);
 
         # We put the states here so that it doesn't need to recompute the forward problem.
         opt.data[:states] = output.states
@@ -60,6 +91,13 @@ function ChainRulesCore.rrule(::typeof(simulate_ad), state0, model, tstep, param
 
         # 3. We have Jutul compute the gradient.
         dparameters = opt.dF!(similar(opt.x0), opt.x0)
+
+        # dparameters_t = deepcopy(parameters)
+        dparameters_t = Dict{Symbol, Any}(
+            k => zeros(v.n)
+            for (k, v) in pairs(opt.data[:mapper])
+        )
+        devectorize_variables!(dparameters_t, model, dparameters, opt.data[:mapper], config = opt.data[:config])
 
         dsimulate = NoTangent()
         dstate0 = NoTangent()
